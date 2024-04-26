4d4547a79437f3ca701952e3f1e0b72667fbd32c
abimbola7 <abimbolababajide6@gmail.com>
2024-04-26 15:42:16 +0100

    Update chart and sidebar components

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({ className, title, amount, firstSrc, secondSrc }) => {
   return (
-    <div className={`p-2 sm:py-7 sm:px-5 ${className} rounded-xl shadow-lg flex flex-col-reverse gap-y-5 sm:flex-row gap-x-5`}>
+    <div className={`p-3 sm:py-7 sm:px-5 ${className} rounded-xl shadow-lg flex flex-col-reverse gap-y-5 sm:flex-row gap-x-5`}>
       <div className='relative w-full sm:w-fit'>
         <img src={firstSrc} alt="curve" className='absolute w-full sm:w-fit' />
         <img src={secondSrc} alt="curve" className='w-full sm:w-fit' />
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -6,24 +6,24 @@ import { IoWalletSharp } from "react-icons/io5";
 
 const SideBar = () => {
   return (
-    <div className='tab:w-20 tab:h-screen p-3 tab:sticky tab:top-0 tab:left-0 transition-all duration-500 w-full h-20 fixed left-0 bottom-0 bg-[#3326AE] tab:bg-transparent z-[10000] rounded-[30px]'>
+    <div className='tab:w-20 tab:h-screen p-1 tab:p-3 tab:sticky tab:top-0 tab:left-0 transition-all duration-500 w-full h-12 sm:h-20 fixed left-0 bottom-0 bg-[#3326AE] tab:bg-transparent z-[10000] rounded-[30px]'>
       {/* <div className='flex item-center justify-center py-4'>
       </div> */}
       <div className='w-full h-full flex flex-row tab:flex-col items-center'>
         <img src="/icon.png" alt="" className='w-10 h-10 mt-4 hidden tab:block'/>
         <div className='w-full h-full flex items-center justify-center'>
-          <div className='w-full flex flex-row tab:flex-col items-center tab:items-stretch justify-center gap-x-12 tab:space-x-0'>
+          <div className='w-full flex flex-row tab:flex-col items-center tab:items-stretch justify-center gap-x-6 sm:gap-x-12 tab:space-x-0'>
             <div className='flex items-center justify-center px-3 py-6 cursor-pointer'>
-              <IoMdHome size={28} color='#857DCE'/>
+              <IoMdHome color='#857DCE' className='text-xl sm:text-3xl'/>
             </div>
             <div className='flex items-center justify-center px-3 py-6 cursor-pointer'>
-              <AiFillPieChart size={24} color='#857DCE'/>
+              <AiFillPieChart color='#857DCE' className='text-xl sm:text-2xl'/>
             </div>
             <div className='flex items-center justify-center px-3 py-6 cursor-pointer'>
-              <MdChatBubble size={24} color='#857DCE'/>
+              <MdChatBubble color='#857DCE' className='text-xl sm:text-2xl'/>
             </div>
             <div className='flex items-center justify-center px-3 py-6 cursor-pointer'>
-              <IoWalletSharp size={24} color='#857DCE'/>
+              <IoWalletSharp color='#857DCE' className='text-xl sm:text-2xl'/>
             </div>
           </div>
         </div>
