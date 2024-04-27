8cf671101638586a3a745a0ed3dfff3774d8c469
abimbola7 <abimbolababajide6@gmail.com>
2024-04-27 09:59:05 +0100

    Update image sizes in Container component

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -4,8 +4,8 @@ const Container = ({ className, title, amount, firstSrc, secondSrc }) => {
   return (
     <div className={`p-3 sm:py-7 sm:px-5 ${className} rounded-xl shadow-lg flex flex-col-reverse gap-y-5 sm:flex-row gap-x-5`}>
       <div className='relative w-full sm:w-fit'>
-        <img src={firstSrc} alt="curve" className='absolute w-full sm:w-fit' />
-        <img src={secondSrc} alt="curve" className='w-full sm:w-fit' />
+        <img src={firstSrc} alt="curve" className='absolute w-full sm:w-16' />
+        <img src={secondSrc} alt="curve" className='w-full sm:w-16' />
       </div>
       <div className={`flex flex-col`}>
         <p className={`text-xs font-medium uppercase tracking-wide ${className === "bg-[#FF392B]" ? "text-[#FFFFFF]" : "text-[#C4BDD5]"}`}>{title}</p>
