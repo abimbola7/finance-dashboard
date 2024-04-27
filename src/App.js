6f03dc0229eaa6ad827abf4b04c36b90346bbdce
abimbola7 <abimbolababajide6@gmail.com>
2024-04-27 08:51:23 +0100

    Refactor UI layout and add responsive behavior

--- src/App.js
+++ src/App.js
@@ -27,6 +27,12 @@ function App() {
       height: window.innerHeight
     });
   };
+  
+  const changeColor = (size, type) => {
+    return windowSize.width <= 768 ? (
+      status === type ? "w-full" : "hidden"
+    ) : size
+  }
 
   React.useEffect(() => {
     window.addEventListener('resize', handleResize);
@@ -51,39 +57,34 @@ function App() {
               windowSize.width <= 768 && (
                 status === "stats" ? "bg-white" : "bg-[#F7F8FF]"
               )}`}>
-            <div
-            onClick={()=>setStatus("stats")}
-            className="cursor-pointer">
-              <LuBarChart 
-              size={20} 
-              color='black' 
-              />
-            </div>
-            <div
-            onClick={()=>setStatus("card")} 
-            className="cursor-pointer">
-              <CiCreditCard1 
-              size={20} 
-              color='black' 
-              />
-            </div>
+                <div className='flex flex-row space-x-5 items-center bg-white px-6 py-2 shadow-2xl rounded-xl'>
+                  <div
+                  onClick={()=>setStatus("stats")}
+                  className="cursor-pointer">
+                    <LuBarChart 
+                    size={20} 
+                    color='black' 
+                    />
+                  </div>
+                  <div
+                  onClick={()=>setStatus("card")} 
+                  className="cursor-pointer">
+                    <CiCreditCard1 
+                    size={20} 
+                    color='black' 
+                    />
+                  </div>
+                </div>
           </div>
           <div className='w-full h-full flex flex-row gap-x-3 max-w-[100%] md:max-w-[95%] xl:max-w-[90%] mx-auto md:mt-8 pb-20 tab:pb-0'>
-            <div className={`px-4 space-y-10 ${
-              windowSize.width <= 768 ? (
-                status === "stats" ? "w-full" : "hidden"
-              ) : 'w-3/4'
+            <div className={`px-4 space-y-10 ${changeColor("w-3/4", "stats")}
             }`}>
               <FirstSection />
               <Chartss />
               <BottomSection />
 
             </div>
-            <div className={`w-1/4 bg-[#F7F8FF] rounded-2xl px-4 py-10 space-y-12 ${
-              windowSize.width <= 768 ? (
-                status === "card" ? "w-full" : "hidden"
-              ) : 'w-1/4'
-            }`}>
+            <div className={`w-1/4 bg-[#F7F8FF] rounded-2xl px-4 py-10 space-y-12 ${changeColor("w-1/4", "card")}`}>
               <RadialChart />
               <DebCard />
               <Transaction />
